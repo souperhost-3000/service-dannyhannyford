be4446ca2a1b9a733a76a08e29000f85f4109594
dannyhannyford <dannyhannyford@gmail.com>
2020-09-25 15:15:45 -0700

    ReviewAverage displays up to 2 decimal places. Price has a minimum of 50

--- database/seed.js
+++ database/seed.js
@@ -15,6 +15,8 @@ const arrangement = ['Hotel room',
 
 const sampleData = [];
 
+const minMaxRandom = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
+
 for (let i = 1; i <= 100; i += 1) {
   const randomArrangement = Math.floor((arrangement.length * Math.random()));
   sampleData.push({
@@ -22,9 +24,9 @@ for (let i = 1; i <= 100; i += 1) {
     place_title: `${arrangement[randomArrangement]} on ${faker.address.streetName()}`,
     sleeping_arrangement: `${faker.company.catchPhraseAdjective()} ${faker.commerce.department()} · ${Math.floor(Math.random() * 5 + 1)} ${faker.random.word()}`,
     image: `https://souperhost.s3-us-west-2.amazonaws.com/stay${i}.jpg`,
-    price: Math.floor(Math.random() * 1000 + 1),
-    review_count: Math.floor(Math.random() * 500),
-    review_average: Math.round((Math.random() * 5) * 10) / 10,
+    price: minMaxRandom(50, 1000),
+    review_count: minMaxRandom(0, 500),
+    review_average: Math.round((Math.random() * 5) * 100) / 100,
     superhost: faker.random.boolean(),
     saved: false,
   });

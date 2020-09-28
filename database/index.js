f0a0d4449b604b052866ae698d172469cfc0c4bc
dannyhannyford <dannyhannyford@gmail.com>
2020-09-27 23:01:45 -0700

    log message more precise

--- database/index.js
+++ database/index.js
@@ -5,7 +5,7 @@ const mongoUri = 'mongodb://localhost:27017/airbnb';
 
 const db = mongoose.connect(mongoUri,
   { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('connected to db'))
+  .then(() => console.log('connected to airbnb db'))
   .catch((err) => console.log(err));
 
 module.exports = db;

b5b95226373ee9acc6d37c67797358a58731b701
dannyhannyford <dannyhannyford@gmail.com>
2020-09-27 22:01:42 -0700

    fixing route test

--- client/src/tests/routes.test.ts
+++ client/src/tests/routes.test.ts
@@ -1,12 +1,15 @@
+/*
+@jest-environment node
+*/
 const request = require('supertest');
 const app = require('../../../server');
+const regeneratorRuntime = require('regenerator-runtime');
 
 describe('Get Endpoints', () => {
   it('should grab an array of listings', async () => {
     const res = await request(app)
       .get('/api/listings');
-    expect(res.body).toHaveLength(1);
+    expect(res.body).toHaveLength(100);
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('listing_id');
   });
 });

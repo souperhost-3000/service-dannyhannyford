2ab2c73da14c3501ff56c479d159746673594f2b
dannyhannyford <dannyhannyford@gmail.com>
2020-09-27 22:58:46 -0700

    basic server/db testing for api calls. No test for seeding. service worker not closing properly.

--- client/src/tests/routes.test.ts
+++ client/src/tests/routes.test.ts
@@ -1,15 +1,29 @@
 /*
 @jest-environment node
 */
+const regeneratorRuntime = require('regenerator-runtime');
 const request = require('supertest');
 const app = require('../../../server');
-const regeneratorRuntime = require('regenerator-runtime');
+const mongoose = require('mongoose');
+
+
 
 describe('Get Endpoints', () => {
+  beforeAll(async () => {
+    const mongoURL = 'mongodb://localhost:27017/airbnb';
+    await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .catch((err) => console.log(err))
+  });
+
   it('should grab an array of listings', async () => {
-    const res = await request(app)
-      .get('/api/listings');
+    const res = await request(app).get('/api/listings');
     expect(res.body).toHaveLength(100);
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it('should grab listing 1', async () => {
+    const res = await request(app).get('/api/listings/1');
+    expect(res.body.listing_id).toEqual(1);
     expect(res.statusCode).toEqual(200);
   });
 });

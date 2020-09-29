4f760469d69e2018e6445d18e4c772d65f149680
dannyhannyford <dannyhannyford@gmail.com>
2020-09-29 12:41:47 -0700

    sample data chnaged to all pictures

--- client/src/components/sampleData.js
+++ client/src/components/sampleData.js
@@ -1,5 +1,9 @@
-const sampleData = [{
-  review_count: 193, review_average: 2.11, listing_id: 17, place_title: 'Entire apartment on Lambert Rapid', sleeping_arrangement: 'Phased Games · 3 enterprise', image: 'https://souperhost.s3-us-west-2.amazonaws.com/stay17.jpg', price: 426, superhost: true, saved: false,
-}];
+// const sampleData = [{
+//   review_count: 193, review_average: 2.11, listing_id: 17, place_title: 'Entire apartment on Lambert Rapid', sleeping_arrangement: 'Phased Games · 3 enterprise', image: 'https://souperhost.s3-us-west-2.amazonaws.com/stay17.jpg', price: 426, superhost: true, saved: false,
+// }];
+const sampleData = [];
+for (let i = 1; i <= 100; i += 1) {
+  sampleData.push(`https://souperhost.s3-us-west-2.amazonaws.com/stay${i}.jpg`);
+}
 
 export default sampleData;

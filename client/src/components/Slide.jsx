3cdf8ff54042bd455b3715ecbc6adb2388e74268
dannyhannyford <dannyhannyford@gmail.com>
2020-09-29 19:25:07 -0700

    trying grid for my slider

--- client/src/components/Slide.jsx
+++ client/src/components/Slide.jsx
@@ -5,16 +5,16 @@ const Slide = ({ slide }) => (
   <div className="slideBlock">
     {/* {console.log(slide)} */}
     <div className="slide">
-      <img src={slide[0]} alt="house" />
+      <img className="image" src={slide[0]} alt="house" />
     </div>
     <div className="slide">
-      <img src={slide[1]} alt="house" />
+      <img className="image" src={slide[1]} alt="house" />
     </div>
     <div className="slide">
-      <img src={slide[2]} alt="house" />
+      <img className="image" src={slide[2]} alt="house" />
     </div>
     <div className="slide">
-      <img src={slide[3]} alt="house" />
+      <img className="image" src={slide[3]} alt="house" />
     </div>
   </div>
 );

ad44b5d300990f245e6e3f7d78f27b6981c02c40
dannyhannyford <dannyhannyford@gmail.com>
2020-09-29 23:15:45 -0700

    static slide info

--- client/src/components/sampleData.js
+++ client/src/components/sampleData.js
@@ -3,7 +3,14 @@
 // }];
 const sampleData = [];
 for (let i = 1; i <= 100; i += 1) {
-  sampleData.push(`https://souperhost.s3-us-west-2.amazonaws.com/stay${i}.jpg`);
+  sampleData.push({
+    image: `https://souperhost.s3-us-west-2.amazonaws.com/stay${i}.jpg`,
+    review_count: 145,
+    review_average: 4.85,
+    price: 426,
+    sleeping_arrangement: 'Phased Games · 3 enterprise',
+    place_title: 'Entire apartment on Lambert Rapid',
+  });
 }
 
 export default sampleData;

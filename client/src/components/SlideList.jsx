f9a5acf12d7c6b22e513847aa244461adfe3698a
dannyhannyford <dannyhannyford@gmail.com>
2020-10-01 15:49:41 -0700

    toggleSave updates a stay's save status on click

--- client/src/components/SlideList.jsx
+++ client/src/components/SlideList.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Slide from './Slide';
 import '../style.css';
 
-const SlideList = ({ slides, move }) => (
+const SlideList = ({ slides, move, toggleSave }) => (
   <ul className="slideContainer" style={move}>
     {slides.map((slide, idx) => (
       <Slide
         // eslint-disable-next-line react/no-array-index-key
         key={idx}
         slide={slide}
+        toggleSave={toggleSave}
       />
     ))}
   </ul>

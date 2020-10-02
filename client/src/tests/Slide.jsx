881ab6d69aa1a34ffdad2bd1b05712b874819b18
dannyhannyford <dannyhannyford@gmail.com>
2020-10-01 19:37:13 -0700

    removing unused files

--- client/src/tests/Slide.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Slide from '../components/Carousel';
-
-describe('A suite', () => {
-  it('renderes without crashing', () => {
-    shallow(<Slide />);
-  });
-});
--- client/src/tests/SlideLIst.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import SlideList from '../components/Carousel';
-
-describe('A suite', () => {
-  it('renderes without crashing', () => {
-    shallow(<SlideList />);
-  });
-});

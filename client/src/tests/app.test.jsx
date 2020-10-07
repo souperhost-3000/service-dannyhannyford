45c9db2c348decef9b90f15250955427c77399c7
dannyhannyford <dannyhannyford@gmail.com>
2020-10-07 09:32:32 -0700

    Test fixed for css change

--- client/src/tests/app.test.jsx
+++ client/src/tests/app.test.jsx
@@ -5,6 +5,12 @@ import Carousel from '../components/Carousel';
 
 describe('App suite', () => {
   it('should render without throwing an error', () => {
-    expect(shallow(<App />).contains(<div><Carousel /></div>)).toBe(true);
+    expect(shallow(<App />).contains(
+      <div className="bodyWrapper">
+        <div className="wrapper">
+          <Carousel />
+        </div>
+      </div>,
+    )).toBe(true);
   });
 });

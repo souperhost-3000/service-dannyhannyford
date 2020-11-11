9ce3d2a4b9c9f6fe57fbae04ecdc066941023882
dannyhannyford <dannyhannyford@gmail.com>
2020-11-10 22:07:15 -0800

    test checks get route for listing 1

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,13 +4,6 @@ const app = require('../index.js');
 
 const request = supertest(app);
 describe('Server Test Suite', () => {
-  it('should handle a GET request to /api/listings', async (done) => {
-    const res = await request.get('/api/listings');
-    expect(res.body).toHaveLength(100);
-    expect(res.status).toBe(200);
-    done();
-  });
-
   it('should handle a GET request for one listing from /api/listings/1', async (done) => {
     const res = await request.get('/api/listings/1');
     expect(res.body.listing_id).toBe(1);

83f2039ffa6f0e0cc36689e6bddd0eca361e3479
dannyhannyford <dannyhannyford@gmail.com>
2020-11-11 14:25:23 -0800

    test to reflect changes to server

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -6,7 +6,7 @@ const request = supertest(app);
 describe('Server Test Suite', () => {
   it('should handle a GET request for one listing from /api/listings/1', async (done) => {
     const res = await request.get('/api/listings/1');
-    expect(res.body.listing_id).toBe(1);
+    expect(res.body.length).toBe(12);
     expect(res.status).toBe(200);
     done();
   });
